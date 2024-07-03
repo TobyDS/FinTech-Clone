160834a72759690ea87222724d2d496d45cc9a5d
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-07-03 14:11:16 +0100

    feat: add api routes

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 const Page = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/listings');
+      const data = await res.json();
+      console.log('🚀 ~ file: crypto.tsx:9 ~ fetchData ~ data:', data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <View>
       <Text>Page</Text>
---
+++ interfaces/crypto.ts
@@ -0,0 +1,55 @@
+export interface Currency {
+  id: number;
+  name: string;
+  symbol: string;
+  slug: string;
+  num_market_pairs: number;
+  date_added: Date;
+  tags: string[];
+  market_cap_by_total_supply?: number;
+  max_supply: number | null;
+  circulating_supply: number;
+  total_supply: number;
+  infinite_supply: boolean;
+  platform: Platform | null;
+  cmc_rank: number;
+  self_reported_circulating_supply: number | null;
+  self_reported_market_cap: number | null;
+  tvl_ratio?: number | null;
+  last_updated: Date;
+  quote: Quote;
+}
+
+export interface Platform {
+  id: number;
+  name: string;
+  symbol: string;
+  slug: string;
+  token_address: string;
+}
+
+export interface Quote {
+  GBP: Gbp;
+}
+
+export interface Gbp {
+  price: number;
+  volume_24h: number;
+  volume_change_24h: number;
+  volume_24h_reported?: number;
+  volume_7d?: number;
+  volume_7d_reported?: number;
+  volume_30d?: number;
+  volume_30d_reported?: number;
+  percent_change_1h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_60d: number;
+  percent_change_90d: number;
+  market_cap: number;
+  market_cap_dominance: number;
+  fully_diluted_market_cap: number;
+  tvl: number | null;
+  last_updated: Date;
+}
